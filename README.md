f4800663cd2b25835398c1f8f267b9fc5917ff93
Imam Assidiqqi <Imam@iniilmu.com>
2016-12-03 19:07:32 +0700

    Init commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Imam.tech Experiments - Monty Hall Problems
+=======================
+
+Monty hall is a popular math problem that has been around for several years. This Ruby program will let you to run several Monty Hall experiments at one click. Just like the real game, the computer won't know anyhing about the winning door and I try to make the program as fair as possible so you won't feel cheated.
+
+Feel free to play on (_link isn't available yet_) or download it and try to run the program yourself.
+
+## Packages that I used
+----------------------
+
+- Ruby
+- Sinatra
+- Sinatra-contrib
+- Vue.js
+- Fetch Polyfill
+- Mudah.css
+- Skeleton.css
+- Normalize
+
+## MIT
+------
+
+The MIT License (MIT)
+Copyright (c) 2016 Imam Assidiqqi (Imam.tech)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ app.rb
@@ -0,0 +1,124 @@
+require 'sinatra'
+require 'json'
+require 'sinatra/json'
+
+class SingleGame
+
+	attr_accessor :winningDoor, :firstDoor,:currentDoor,:willChangeDoor,:losingDoor,:isWinning
+
+	def initialize(willChangeDoor)
+		@winningDoor = randomize
+		@firstDoor = randomize	
+		@currentDoor = @firstDoor	
+		@willChangeDoor = willChangeDoor
+		openLosingDoor
+		if(@willChangeDoor == true)
+			changeDoor
+		end
+		if(@currentDoor == @winningDoor)
+			@isWinning = true
+		else
+			@isWinning = false
+		end
+	end
+
+	def openLosingDoor
+		@losingDoor = randomize
+		if(@losingDoor == @winningDoor || @losingDoor == @currentDoor)
+			openLosingDoor
+		end
+	end
+
+	def changeDoor
+		@currentDoor = randomize
+		if(@currentDoor == @losingDoor || @currentDoor == @firstDoor)
+			changeDoor
+		end
+	end
+
+	def to_json(*a)
+		{winningDoor: @winningDoor,firstDoor: @firstDoor, willChangeDoor: @willChangeDoor,isWinning: @isWinning}.to_json(*a)
+	end
+
+	private
+	def randomize
+		return Random.rand(3)
+	end
+
+end
+
+class GameCounter
+
+	attr_accessor :winningTotal, :totalGames, :winningPercentage
+
+	def initialize
+		@totalGames	= 0
+		@winningTotal = 0
+		@winningPercentage = 0
+	end
+
+	def addGame(game)
+		@totalGames	= @totalGames + 1
+		@winningTotal = @winningTotal + 1 if game.isWinning == true
+		@winningPercentage = @winningTotal.to_f / @totalGames.to_f * 100
+	end
+
+end
+
+class GameWatcher
+
+	attr_accessor :games,:winningTotal, :winningPercentage, :totalGames
+
+	def initialize
+		@counter = GameCounter.new
+		@games = []
+	end
+
+	def addGame(game)
+		@games << game
+		@counter.addGame(game)	
+		@winningTotal = @counter.winningTotal
+		@winningPercentage = @counter.winningPercentage
+		@totalGames = @counter.totalGames
+	end
+
+end
+
+class GameRepetition
+
+	attr_accessor :games,:winningTotal, :winningPercentage
+
+	def initialize(totalGames, willChangeDoor)
+		@watcher = GameWatcher.new
+		@games = @watcher.games
+		@winningTotal = @watcher.winningTotal
+		@winningPercentage = @watcher.winningPercentage
+	end
+
+	def startGame(totalGames, willChangeDoor)
+		totalGames.times do
+			game = SingleGame.new(willChangeDoor)
+			@watcher.addGame(game)
+		end
+		@games = @watcher.games
+		@winningTotal = @watcher.winningTotal
+		@winningPercentage = @watcher.winningPercentage
+	end
+
+	def to_json(*a)
+		{games: @games, winningTotal: @winningTotal, winningPercentage: @winningPercentage}.to_json(*a)
+	end
+end
+
+
+
+get '/' do
+	erb :index 
+end
+
+get '/game' do
+	games = GameRepetition.new(params['change_door_repetition'].to_i, true)
+	games.startGame(params['change_door_repetition'].to_i,true)
+	games.startGame(params['dont_change_door_repetition'].to_i, false)
+	json games
+end
